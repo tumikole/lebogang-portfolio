05cd59216cd54445e5a02e8925427ca6c0903411
Samuel Ramollo <samuelramollo@MacBookPro.lan>
2024-11-25 23:42:17 +0200

    created a new project

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Vid1 from "./Asserts/vid1.mp4";
+import Vid2 from "./Asserts/vid2.mp4";
+import Vid3 from "./Asserts/vid3.mp4";
 
 function App() {
+  const [activeVideo, setActiveVideo] = useState(null); // Tracks the active video
+
+  const videos = [Vid1, Vid2, Vid3];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{textAlign:"center"}}>Hi, Lebz</h1>
+      <button
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        className="btn btn-success"
+        onClick={() => setActiveVideo(0)} // Play Video 1
+      >
+        Play Video 1
+      </button>
+      <button
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        className="btn btn-success"
+        onClick={() => setActiveVideo(1)} // Play Video 2
+      >
+        Play Video 2
+      </button>
+      <button
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        className="btn btn-success"
+        onClick={() => setActiveVideo(2)} // Play Video 3
+      >
+        Play Video 3
+      </button>
+
+      <div style={{marginTop: "3rem",marginLeft: "3rem",marginRight: "3rem"}}>
+        {activeVideo !== null && (
+          <video
+            key={activeVideo} // Forces React to recreate the element when the key changes
+            style={{
+              width: "100%",
+              border: "5px solid blue",
+              borderRadius: "10px",
+              boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
+              backgroundColor: "black",
+              height: "80vh",
+            }}
+            controls={false}
+            autoPlay
+          >
+            <source src={videos[activeVideo]} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+      </div>
     </div>
   );
 }
--- user-interface/src/index.js
+++ user-interface/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
